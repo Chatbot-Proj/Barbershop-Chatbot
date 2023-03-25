6d01c5002a59a24256c07fa45363ce52f716b9ac
Aimen Altaiyeb <aaltaiyeb@godaddy.com>
2023-03-24 22:25:26 -0700

    Add capability to ask for available appointments.

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,41 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+import json
+from typing import Any, Text, Dict, List
+from pathlib import Path
+from prettytable import PrettyTable
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionAvailableAppointments(Action):
+    def name(self) -> Text:
+        return "action_available_appointments"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Load the JSON file with the available appointments
+        file_path = str(Path(__file__).parent / "sample_data/available_times.json")
+        with open(file_path) as f:
+            data = json.load(f)
+
+        # Extract the appointment data from the JSON file
+        appointments = data["appointments"]
+
+        # Create a table with the appointment data
+        table = PrettyTable()
+        table.field_names = ["Date", "Time", "Length", "Barber Name", "Location"]
+
+        for appointment in appointments:
+            table.add_row(
+                [
+                    appointment["date"],
+                    appointment["time"],
+                    appointment["length"],
+                    appointment["barber_name"],
+                    appointment["location"]["address"],
+                ]
+            )
+
+        # Send the table back to the user
+        dispatcher.utter_message(text=f"Here are the available appointment times:\n\n{table}")
+
+        return []
